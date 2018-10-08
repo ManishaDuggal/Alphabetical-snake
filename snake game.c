4c1703ee91250b771cec3a05059fbb555b58822b
Manisha <manishaduggal999@gmail.com>
2018-10-08 12:28:49 +0530

    used kbhit

--- snake game.c
+++ snake game.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include<time.h>
+#include<conio.h>
 struct food{
 int row,col;
 char value;
@@ -12,43 +13,64 @@ char value;
 struct snake *next;
 struct snake *pre;
 };
-
+void delay(unsigned int mseconds)
+{
+    clock_t goal = mseconds + clock();
+    while (goal > clock());
+}
 void insertionAtEnd(struct snake **head,struct snake **tail,int r,int c,char value);
 void Display(struct snake *ptr);
 void move(struct snake **head,struct snake **tail,char w,struct food *arr);
 void insertionAtEnd1(struct snake *head,struct snake *tail,int r,int c,char value);
 void generate_food(struct snake **head,struct snake **tail,struct food *arr,int c);
 void clean_board(char board[][20],struct food *arr);
+void write_in_file(struct snake *head,struct snake *tail,struct food *arr);
+void file_handling(struct snake **head,struct snake **tail,struct food *arr);
 int main(){
-
+system("COLOR F0");
 struct snake *start=NULL,*tail=NULL;
-insertionAtEnd(&start,&tail,3,2,'A');
-insertionAtEnd(&start,&tail,4,2,'B');
-insertionAtEnd(&start,&tail,5,2,'C');
-insertionAtEnd(&start,&tail,6,2,'D');
-insertionAtEnd(&start,&tail,7,2,'E');
-insertionAtEnd(&start,&tail,8,2,'F');
-
-///generating food
 struct food arr[10];
-int i;
+FILE *fp=fopen("snake.c","r");
+char check=getc(fp);
+fclose(fp);
+if(check!=EOF){
+    printf("\nDo you wanna continue with existing game y/n?");
+    char ch;
+    scanf(" %c",&ch);
+    if(ch=='y'){
+     file_handling(&start,&tail,arr);
+    }else{
+    insertionAtEnd(&start,&tail,3,2,'A');
+    insertionAtEnd(&start,&tail,4,2,'B');
+    insertionAtEnd(&start,&tail,5,2,'C');
+    insertionAtEnd(&start,&tail,6,2,'D');
+    insertionAtEnd(&start,&tail,7,2,'E');
+    insertionAtEnd(&start,&tail,8,2,'F');
+
+      int i;
     for(i=0;i<10;i++){
        generate_food(&start,&tail,arr,i);
+       arr[i].value='Z'+i;
+    }
+       }
+}else{
+
+    insertionAtEnd(&start,&tail,3,2,'A');
+    insertionAtEnd(&start,&tail,4,2,'B');
+    insertionAtEnd(&start,&tail,5,2,'C');
+    insertionAtEnd(&start,&tail,6,2,'D');
+    insertionAtEnd(&start,&tail,7,2,'E');
+    insertionAtEnd(&start,&tail,8,2,'F');
+
+      int i;
+     for(i=0;i<10;i++){
+       generate_food(&start,&tail,arr,i);
        arr[i].value='G'+i;
+      }
     }
 
+
 char board[20][20];
-int j;
-///boundary rows
-for(i=0;i<20;i++){
-    board[0][i]='-';
-     board[19][i]='-';
-}
-///boundary cols
-for(i=0;i<20;i++){
-    board[i][0]='|';
-     board[i][19]='|';
-}
 
 clean_board(board,arr);
 
@@ -57,6 +79,7 @@ while(temp){
     board[temp->row][temp->col]=temp->value;
     temp=temp->next;
 }
+int j,i;
 for(i=0;i<20;i++){
     for(j=0;j<20;j++){
         printf("%c",board[i][j]);
@@ -64,12 +87,35 @@ for(i=0;i<20;i++){
     printf("\n");
 }
 int k;
-for(k=0;k<1000;k++){
-printf("move ");
 char n;
-scanf("%c",&n);
-getchar();
+n='w';
+while(1){
+        if(start->next==NULL){
+            system("CLS");
+            printf("You Lost");
+           return 0;
+        }
+        if(tail->value=='Z'){
+            system("CLS");
+            printf("Congratulations for winning");
+            FILE *filepoint=fopen("snake.c","w");
+             fclose(filepoint);
+            break;
+        }
+if(kbhit()){
+    n=getch();
+}
+if(n=='q'){
+    printf("\nDo you wanna save state?y/n");
+    getchar();
+   n=getchar();
+   if(n=='y'){
+    write_in_file(start,tail,arr);
+   }
+    return 0;
+}
 clean_board(board,arr);
+delay(459);
 move(&start,&tail,n,arr);
 system("CLS");
 temp=start;
@@ -111,41 +157,76 @@ void Display(struct snake *ptr){
 }
 
 void move(struct snake **head,struct snake **tail,char w,struct food *arr){
-    struct snake *temp=*tail;
+    struct snake *temp;
+    int flag=0;
 
-if(w=='t'){
+if(w=='w'){
      temp=(*head)->next;
-    while(temp&&(temp->row!=(*head)->row-1||temp->col!=(*head)->col)){
+     if((*head)->row-1>=1){
+    while(temp){
+        if(temp->row==(*head)->row-1&&temp->col==(*head)->col){
+            flag=1;
+            break;
+        }
         temp=temp->next;
-    }
-    if(temp!=NULL){
-        printf("\a");
-        return ;
-    }
-    temp=(*tail);
+            }
 
+        if(flag==1){
+        printf("1\a");
+        printf("flag1");
+        return ;
+                 }
+
+    temp=(*tail);
     while(temp->pre){
        temp->row=temp->pre->row;
        temp->col=temp->pre->col;
        temp=temp->pre;
     }
-  if((*head)->row-1>=1){
-     (*head)->row=(*head)->row-1;
-   }else{
-     (*head)->row=18;
+      (*head)->row=(*head)->row-1;
+     }else{
+         while(temp){
+          if(temp->row==18&&temp->col==(*head)->col){
+            flag=1;
+            break;
+          }
+         temp=temp->next;
+         }
+
+        if(flag==1){
+        printf("2\a");
+        printf("flag1");
+        return ;
+          }
+
+    temp=(*tail);
+    while(temp->pre){
+       temp->row=temp->pre->row;
+       temp->col=temp->pre->col;
+       temp=temp->pre;
+    }
+
+    (*head)->row=18;
      if((*head)->col+1<=18)
      (*head)->col=(*head)->col+1;
      else
         (*head)->col=1;
-       }
-}else if(w=='r'){
-    temp=(*head)->next;
-    while(temp&&(temp->col!=(*head)->col+1||temp->row!=(*head)->row)){
+     }
+
+}else if(w=='d'){
+
+ if((*head)->col+1<=18){
+        temp=(*head)->next;
+    while(temp){
+        if(temp->col==(*head)->col+1&&temp->row==(*head)->row){
+            flag=1;
+            break;
+        }
         temp=temp->next;
     }
-    if(temp!=NULL){
-        printf("\a");
-        return ;
+    if(flag==1){
+        printf("3\a");
+        return;
     }
     temp=(*tail);
     while(temp->pre){
@@ -153,43 +234,90 @@ if(w=='t'){
        temp->col=temp->pre->col;
        temp=temp->pre;
     }
- if((*head)->col+1<=18){
      (*head)->col=(*head)->col+1;
    }else{
+       temp=(*head)->next;
+    while(temp){
+        if(temp->col==1&&temp->row==(*head)->row){
+            flag=1;
+            break;
+        }
+        temp=temp->next;
+    }
+    if(flag==1){
+        printf("4\a");
+        return;
+    }
+    temp=(*tail);
+    while(temp->pre){
+       temp->row=temp->pre->row;
+       temp->col=temp->pre->col;
+       temp=temp->pre;
+    }
      (*head)->col=1;
      (*head)->row=(*head)->row+1;
        }
-    }else if(w=='l'){
+    }else if(w=='a'){
 
-    temp=(*head)->next;
-    while(temp&&(temp->col!=(*head)->col-1||temp->row!=(*head)->row)){
+
+    if((*head)->col-1>=1){
+
+     temp=(*head)->next;
+    while(temp){
+        if(temp->col==(*head)->col-1&&temp->row==(*head)->row){
+            flag=1;
+            break;
+        }
         temp=temp->next;
     }
-    if(temp!=NULL){
-        printf("\a");
-        return ;
+    if(flag==1){
+        printf("5\a");
+        return;
     }
     temp=(*tail);
-    ///changing coordinates except of head node
+
     while(temp->pre){
        temp->row=temp->pre->row;
        temp->col=temp->pre->col;
        temp=temp->pre;
     }
-    if((*head)->col-1>=1){
      (*head)->col=(*head)->col-1;
      }else{
+         temp=(*head)->next;
+    while(temp){
+        if(temp->col==18&&temp->row==(*head)->row){
+            flag=1;
+            break;
+        }
+        temp=temp->next;
+    }
+    if(flag==1){
+        printf("6\a");
+        return;
+    }
+    temp=(*tail);
+
+    while(temp->pre){
+       temp->row=temp->pre->row;
+       temp->col=temp->pre->col;
+       temp=temp->pre;
+    }
      (*head)->col=18;
      (*head)->row=(*head)->row+1;
        }
-    }else if(w=='b'){
+    }else if(w=='s'){
 
-    temp=(*head)->next;
-    while(temp&&(temp->row!=(*head)->row+1||temp->col!=(*head)->col)){
+    if((*head)->row+1<=18){
+      temp=(*head)->next;
+    while(temp){
+        if(temp->row==(*head)->row+1&&temp->col==(*head)->col){
+            flag=1;
+            break;
+        }
         temp=temp->next;
     }
-    if(temp!=NULL){
-        printf("\a");
+    if(flag==1){
+        printf("7\a");
         return ;
     }
     temp=(*tail);
@@ -199,14 +327,34 @@ if(w=='t'){
        temp->col=temp->pre->col;
        temp=temp->pre;
     }
-    if((*head)->row+1<=18){
      (*head)->row=(*head)->row+1;
      }else{
+      temp=(*head)->next;
+    while(temp){
+        if(temp->row==1&&temp->col==(*head)->col){
+            flag=1;
+            break;
+        }
+        temp=temp->next;
+    }
+    if(flag==1){
+        printf("8\a");
+        return ;
+    }
+    temp=(*tail);
+
+    while(temp->pre){
+       temp->row=temp->pre->row;
+       temp->col=temp->pre->col;
+       temp=temp->pre;
+    }
      (*head)->row=1;
      (*head)->col=(*head)->col+1;
        }
     }else{
-     printf("\a");
+     printf("9\a");
+     printf("hello dear");
+     return ;
     }
     int i,c=-1;
 
@@ -233,7 +381,7 @@ if(w=='t'){
 
 void generate_food(struct snake **head,struct snake **tail,struct food *arr,int c){
 srand(time(0)*c);
-arr[c].value=(*tail)->value+1;//(rand()%((*tail)->value+5-(*tail)->value+1)+(*tail)->value);
+arr[c].value=(*tail)->value+1;
 again:
 arr[c].row=(rand()%(18-1+1)+1);
 arr[c].col=(rand()%(18-1+1)+1);
@@ -248,6 +396,17 @@ while(temp){
 
 void clean_board(char board[][20],struct food *arr){
     int i,j;
+///boundary rows
+for(i=0;i<20;i++){
+    board[0][i]='-';
+     board[19][i]='-';
+}
+///boundary cols
+for(i=0;i<20;i++){
+    board[i][0]='|';
+     board[i][19]='|';
+}
+
 for(i=1;i<19;i++){
     for(j=1;j<19;j++){
         board[i][j]=' ';
@@ -256,4 +415,93 @@ for(i=1;i<19;i++){
 for(i=0;i<10;i++){
     board[arr[i].row][arr[i].col]=arr[i].value;
 }
+}
+void write_in_file(struct snake *head,struct snake *tail,struct food *arr){
+struct snake *ptr=head;
+FILE *fp;
+fp=fopen("snake.c","w");
+ while(ptr){
+   fputc(ptr->row+'0',fp);
+   fputc(',',fp);
+   fputc(ptr->col+'0',fp);
+   fputc(',',fp);
+   fputc(ptr->value,fp);
+   fputc('|',fp);
+   ptr=ptr->next;
+ }
+ fseek(fp, -1L, SEEK_CUR);
+ fputc('$',fp);
+ int i=0;
+  for(i=0;i<10;i++){
+  char buffer[3];
+  itoa(arr[i].row,buffer,10);
+  fprintf(fp,"%s",buffer);
+   fputc(',',fp);
+   itoa(arr[i].col,buffer,10);
+   fprintf(fp,"%s",buffer);
+   fputc(',',fp);
+   fputc(arr[i].value,fp);
+   fputc('|',fp);
+ }
+ fseek(fp, -1L, SEEK_CUR);
+ fputc(' ',fp);
+ fclose(fp);
+}
+void file_handling(struct snake **head,struct snake **tail,struct food *arr){
+FILE *fp;
+
+fp=fopen("snake.c","r");
+int no;
+char c=getc(fp);
+list:
+   no=0;
+  while(c!=','){
+  no=no*10+(c-'0');
+  c=getc(fp);
+  }
+  int x=no;
+  c=getc(fp);
+  no=0;
+  while(c!=','){
+  no=no*10+(c-'0');
+  c=getc(fp);
+  }
+ int y=no;
+  c=getc(fp);
+  char value=c;
+  c=getc(fp);
+  insertionAtEnd(head,tail,x,y,value);
+
+if(c=='|'){
+c=getc(fp);
+goto list;
+}
+if(c=='$'){
+int i=0;
+c=getc(fp);
+food:
+   no=0;
+  while(c!=','){
+  no=no*10+(c-'0');
+  c=getc(fp);
+  }
+  arr[i].row=no;
+  c=getc(fp);
+  no=0;
+  while(c!=','){
+  no=no*10+(c-'0');
+  c=getc(fp);
+  }
+  arr[i].col=no;
+  c=getc(fp);
+  arr[i++].value=c;
+  c=getc(fp);
+
+if(c=='|'){
+c=getc(fp);
+goto food;
+}
+}
+fclose(fp);
+
 }
